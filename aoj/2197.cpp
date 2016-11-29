17fba6e193d6e1999261a2ccc1bddf961e821def
rdrgn <reminjp@gmail.com>
2016-11-29 17:29:14 +0900

    add aoj/2197.cpp

---
+++ aoj/2197.cpp
@@ -0,0 +1,29 @@
+// 2016-11-25
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	int h, s, c;
+	while (true) {
+		cin >> n;
+		if (n == 0) {
+			break;
+		}
+		h = 1;
+		s = 0;
+		c = 0;
+		for (int i = 1; i < n + 1; i++) {
+			while (s >= n) {
+				if (s == n) {
+					c++;
+				}
+				s -= h;
+				h++;
+			}
+			s += i;
+		}
+		cout << c << endl;
+	}
+	return 0;
+}
