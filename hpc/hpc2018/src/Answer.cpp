bdd0e0f05ec3cfa7db51c3955b728cc0c93a58b6
rdrgn <noreply@remin.jp>
2018-10-04 17:39:54 +0900

    update hpc2018 (score 1847605)

--- hpc/hpc2018/src/Answer.cpp
+++ hpc/hpc2018/src/Answer.cpp
@@ -1,70 +1,58 @@
-﻿//------------------------------------------------------------------------------
-/// @file
-/// @author   ハル研究所プログラミングコンテスト実行委員会
-///
-/// @copyright  Copyright (c) 2018 HAL Laboratory, Inc.
-/// @attention  このファイルの利用は、同梱のREADMEにある
-///             利用条件に従ってください。
-//------------------------------------------------------------------------------
-#include "Answer.hpp"
+﻿#include "Answer.hpp"
+#include <algorithm>
+#include <utility>
 
-//------------------------------------------------------------------------------
 namespace hpc {
 
-//------------------------------------------------------------------------------
-/// コンストラクタ。
-/// @detail 最初のステージ開始前に実行したい処理があればここに書きます。
-Answer::Answer()
-{
-}
+Answer::Answer() {}
 
-//------------------------------------------------------------------------------
-/// デストラクタ。
-/// @detail 最後のステージ終了後に実行したい処理があればここに書きます。
-Answer::~Answer()
-{
-}
+Answer::~Answer() {}
 
-//------------------------------------------------------------------------------
-/// 各ステージ開始時に呼び出される処理。
-/// @detail 各ステージに対する初期化処理が必要ならここに書きます。
-/// @param aStage 現在のステージ。
-void Answer::init(const Stage& aStage)
-{
-}
+void Answer::init(const Stage& aStage) {}
 
-//------------------------------------------------------------------------------
-/// このターンでの行動を指定する。
-/// @detail 希望する行動を Action の static 関数で生成して return してください。
-///         正常ではない行動を return した場合、何も起きません。
-///         詳細は Action のヘッダを参照してください。
-/// @param aStage 現在のステージ。
-Action Answer::decideNextAction(const Stage& aStage)
-{
-    // 解答コードのサンプルです
+Action Answer::decideNextAction(const Stage& aStage) {
+  Array<std::pair<CandidateLaneType, int>, 2 * Parameter::CandidatePieceCount> pieceIndices;
+  for (int i = 0, count = aStage.candidateLane(CandidateLaneType_Small).pieces().count(); i < count; i++) {
+    pieceIndices.add({CandidateLaneType_Small, i});
+  }
+  for (int i = 0, count = aStage.candidateLane(CandidateLaneType_Large).pieces().count(); i < count; i++) {
+    pieceIndices.add({CandidateLaneType_Large, i});
+  }
 
-    // 小さい生地の生地置き場にある、0番目の生地を対象として
-    auto laneType = CandidateLaneType_Small;
-    int pieceIndex = 0;
+  sort(pieceIndices.begin(), pieceIndices.end(),
+       [&aStage](const std::pair<CandidateLaneType, int>& a, const std::pair<CandidateLaneType, int>& b) {
+         auto pieceA = aStage.candidateLane(a.first).pieces()[a.second];
+         auto pieceB = aStage.candidateLane(b.first).pieces()[b.second];
+         int areaA = pieceA.height() * pieceA.width();
+         int areaB = pieceB.height() * pieceB.width();
+         int timeSpaceA = pieceA.requiredHeatTurnCount() * areaA;
+         int timeSpaceB = pieceB.requiredHeatTurnCount() * areaB;
+         double valueA = (double)pieceA.score() / timeSpaceA;
+         double valueB = (double)pieceB.score() / timeSpaceB;
+         return valueA != valueB
+                    ? valueA > valueB
+                    : timeSpaceA != timeSpaceB ? timeSpaceA < timeSpaceB
+                                               : areaA != areaB ? areaA < areaB : pieceA.height() < pieceB.height();
+       });
+
+  for (auto& e : pieceIndices) {
+    CandidateLaneType laneType = e.first;
+    int pieceIndex = e.second;
     const auto& piece = aStage.candidateLane(laneType).pieces()[pieceIndex];
 
-    // オーブンの原点に配置できそうなら、配置する
-    Vector2i putPos(0, 0);
-    if (aStage.oven().isAbleToPut(piece, putPos)) {
-        return Action::Put(laneType, pieceIndex, putPos);
+    Vector2i pos;
+    for (pos.y = 0; pos.y < aStage.oven().height() - piece.height() + 1; pos.y++) {
+      for (pos.x = 0; pos.x < aStage.oven().width() - piece.width() + 1; pos.x++) {
+        if (aStage.oven().isAbleToPut(piece, pos)) {
+          return Action::Put(laneType, pieceIndex, pos);
+        }
+      }
     }
+  }
 
-    // 配置できないなら、このターンは何もしない
-    return Action::Wait();
+  return Action::Wait();
 }
 
-//------------------------------------------------------------------------------
-/// 各ステージ終了時に呼び出される処理。
-/// @detail 各ステージに対する終了処理が必要ならここに書きます。
-/// @param aStage 現在のステージ。
-void Answer::finalize(const Stage& aStage)
-{
-}
+void Answer::finalize(const Stage& aStage) {}
 
-} // namespace
-// EOF
+}  // namespace hpc
