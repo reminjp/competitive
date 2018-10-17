f41673eac8a6102748085c822be7cc3b14a46a04
rdrgn <noreply@remin.jp>
2018-10-18 05:37:48 +0900

    update hpc2018 (score 2448679)

--- hpc/hpc2018/src/Answer.cpp
+++ hpc/hpc2018/src/Answer.cpp
@@ -1,170 +1,101 @@
 ﻿#include "Answer.hpp"
 #include <algorithm>
-#include <numeric>
-#include <utility>
-#include <vector>
 
 namespace hpc {
-
-struct PieceWithTurn {
-  Piece piece;
-  int turn;
-
-  PieceWithTurn(const Piece& p, int t) : piece(p), turn(t) {}
-
-  bool collides(const PieceWithTurn& a) {
-    return this->piece.pos().x < a.piece.pos().x + a.piece.width() &&
-           this->piece.pos().x + this->piece.width() > a.piece.pos().x &&
-           this->piece.pos().y < a.piece.pos().y + a.piece.height() &&
-           this->piece.pos().y + this->piece.height() > a.piece.pos().y &&
-           this->turn <= a.turn + a.piece.requiredHeatTurnCount() &&
-           this->turn + this->piece.requiredHeatTurnCount() >= a.turn;
-  }
-
-  bool collides(const std::vector<PieceWithTurn>& a) {
-    bool r = false;
-    for (auto e : a) {
-      r |= this->collides(e);
-    }
-    return r;
-  }
-};
-
-bool arePiecesHorizontal;
-std::vector<PieceWithTurn> reservedPieces;
 
 Answer::Answer() {}
 
 Answer::~Answer() {}
 
-void Answer::init(const Stage& aStage) {
-  // auto recipe = aStage.candidateLane(CandidateLaneType_Large).recipe();
-  // arePiecesHorizontal = (recipe.foldPosRatioMin() + recipe.foldPosRatioTerm()) / 2.0 > 0.5;
-  reservedPieces.clear();
-}
+void Answer::init(const Stage& aStage) {}
 
 Action Answer::decideNextAction(const Stage& aStage) {
-  // 生地置き場にあるクッキーの形状を調べる。
-  {
-    int verticalPieceCount = 0;
-    for (auto type : {CandidateLaneType_Small, CandidateLaneType_Large}) {
-      for (int i = 0; i < Parameter::CandidatePieceCount; i++) {
-        auto& piece = aStage.candidateLane(type).pieces()[i];
-        verticalPieceCount += piece.width() < piece.height();
-      }
-    }
-    arePiecesHorizontal = verticalPieceCount < Parameter::CandidatePieceCount;
-  }
-
-  // オーブンにあるクッキーを読み取る。
-  std::vector<PieceWithTurn> bakingPieces;
+  // オーブンにある大きいクッキーが焼きあがるまでの時間を取得する。
+  int minRestRequiredHeatTurnCount = Parameter::GameTurnLimit;
   for (auto piece : aStage.oven().bakingPieces()) {
-    bakingPieces.push_back(PieceWithTurn(piece, aStage.turn() - piece.currentHeatTurnCount()));
+    if (piece.width() + piece.height() >= Parameter::MinLargePieceSampleEdgeLength) {
+      minRestRequiredHeatTurnCount = Math::Min(minRestRequiredHeatTurnCount, piece.restRequiredHeatTurnCount());
+    }
   }
 
-  // 大きいクッキーの置き方を予め決めておく。
-  if (!reservedPieces.size()) {
-    // より大きいクッキーを先に試すように並び替える。
-    std::vector<int> order(Parameter::CandidatePieceCount);
-    std::iota(begin(order), end(order), 0);
-    sort(order.begin(), order.end(), [&aStage](int a, int b) {
+  bool gameWillEnd = true;
+
+  // 大きいクッキーを左上から詰めて置く。
+  {
+    // 試すクッキーの順番を変える。
+    Array<int, Parameter::CandidatePieceCount> order;
+    for (int i = 0; i < order.maxCount(); i++) order.add(i);
+    std::sort(order.begin(), order.end(), [&aStage, minRestRequiredHeatTurnCount](int a, int b) {
       auto pieceA = aStage.candidateLane(CandidateLaneType_Large).pieces()[a];
       auto pieceB = aStage.candidateLane(CandidateLaneType_Large).pieces()[b];
-      int sizeA = pieceA.height() * pieceA.width();
-      int sizeB = pieceB.height() * pieceB.width();
+      auto sizeA = pieceA.height() * pieceA.width();
+      auto sizeB = pieceB.height() * pieceB.width();
       return sizeA != sizeB ? sizeA > sizeB : a < b;
     });
 
-    // 大きいクッキーを左上から詰めて置く。
-    int putPieceCount = 0;
-    std::vector<bool> isUsed(Parameter::CandidatePieceCount);
-    for (int t = aStage.turn();
-         putPieceCount < Parameter::CandidatePieceCount && t <= aStage.turn() + Parameter::GameTurnLimit; t++) {
-      bool didPut = false;
+    for (int i = 0; i < Parameter::CandidatePieceCount; i++) {
+      auto& piece = aStage.candidateLane(CandidateLaneType_Large).pieces()[order[i]];
 
-      for (int i = 0; !didPut && i < Parameter::CandidatePieceCount; i++) {
-        if (isUsed[i]) {
-          continue;
+      // ゲーム終了までに焼き終わらないクッキーを置かない。
+      if (piece.requiredHeatTurnCount() >= Parameter::GameTurnLimit - aStage.turn()) {
+        continue;
+      }
+      gameWillEnd = false;
+
+      for (int y = 0; y <= Parameter::OvenHeight - piece.height(); y++) {
+        Vector2i pos;
+        pos = Vector2i(0, y);
+        if (aStage.oven().isAbleToPut(piece, pos)) {
+          return Action::Put(CandidateLaneType_Large, order[i], pos);
         }
-        auto piece = aStage.candidateLane(CandidateLaneType_Large).pieces()[order[i]];
-
-        auto func = [&](int x, int y) {
-          piece.setPos(Vector2i(x, y));
-          PieceWithTurn p(piece, t);
-          if (!p.collides(bakingPieces) && !p.collides(reservedPieces)) {
-            reservedPieces.push_back(p);
-            putPieceCount++;
-            isUsed[i] = true;
-            didPut = true;
-          }
-        };
-        if (arePiecesHorizontal) {
-          for (int y = 0; !didPut && y < Parameter::OvenHeight - piece.height() + 1; y++) {
-            for (int x = 0; !didPut && x < Parameter::OvenWidth - piece.width() + 1; x++) {
-              func(x, y);
-            }
-          }
-        } else {
-          for (int x = 0; !didPut && x < Parameter::OvenWidth - piece.width() + 1; x++) {
-            for (int y = 0; !didPut && y < Parameter::OvenHeight - piece.height() + 1; y++) {
-              func(x, y);
-            }
-          }
+        pos = Vector2i(Parameter::OvenWidth - piece.width(), y);
+        if (aStage.oven().isAbleToPut(piece, pos)) {
+          return Action::Put(CandidateLaneType_Large, order[i], pos);
         }
       }
-    }
-  }
 
-  // キューの先頭にある大きいクッキーを置く。
-  if (reservedPieces.size() && reservedPieces.front().turn == aStage.turn()) {
-    auto piece = reservedPieces.front().piece;
-    reservedPieces.clear();
-
-    for (int i = 0; i < Parameter::CandidatePieceCount; i++) {
-      auto& p = aStage.candidateLane(CandidateLaneType_Large).pieces()[i];
-      if (p.width() == piece.width() && p.height() == piece.height() &&
-          p.requiredHeatTurnCount() == piece.requiredHeatTurnCount() && p.score() == piece.score()) {
-        return Action::Put(CandidateLaneType_Large, i, piece.pos());
+      for (int x = 0; x <= Parameter::OvenWidth - piece.width(); x++) {
+        Vector2i pos;
+        pos = Vector2i(x, 0);
+        if (aStage.oven().isAbleToPut(piece, pos)) {
+          return Action::Put(CandidateLaneType_Large, order[i], pos);
+        }
+        pos = Vector2i(x, Parameter::OvenHeight - piece.height());
+        if (aStage.oven().isAbleToPut(piece, pos)) {
+          return Action::Put(CandidateLaneType_Large, order[i], pos);
+        }
       }
     }
   }
 
   // 小さいクッキーを大きいクッキーと重ならない場所に置く。
   {
-    // よりスコアが高いクッキーを先に試すように並び替える。
-    std::vector<int> order(Parameter::CandidatePieceCount);
-    std::iota(begin(order), end(order), 0);
-    sort(order.begin(), order.end(), [&aStage](int a, int b) {
+    // 試すクッキーの順番を変える。
+    Array<int, Parameter::CandidatePieceCount> order;
+    for (int i = 0; i < order.maxCount(); i++) order.add(i);
+    std::sort(order.begin(), order.end(), [&aStage](int a, int b) {
       auto pieceA = aStage.candidateLane(CandidateLaneType_Small).pieces()[a];
       auto pieceB = aStage.candidateLane(CandidateLaneType_Small).pieces()[b];
-      int scoreA = pieceA.score();
-      int scoreB = pieceB.score();
-      return scoreA != scoreB ? scoreA > scoreB : a < b;
+      auto turnA = pieceA.requiredHeatTurnCount();
+      auto turnB = pieceB.requiredHeatTurnCount();
+      auto scoreA = pieceA.score();
+      auto scoreB = pieceB.score();
+      return turnA != turnB ? turnA > turnB : scoreA != scoreB ? scoreA > scoreB : a < b;
     });
 
     for (int i = 0; i < Parameter::CandidatePieceCount; i++) {
-      auto piece = aStage.candidateLane(CandidateLaneType_Small).pieces()[order[i]];
+      auto& piece = aStage.candidateLane(CandidateLaneType_Small).pieces()[order[i]];
 
-      if (arePiecesHorizontal) {
-        for (int y = 0; y < Parameter::OvenHeight - piece.height() + 1; y++) {
-          for (int x = 0; x < Parameter::OvenWidth - piece.width() + 1; x++) {
-            Vector2i pos = Vector2i(x, y);
-            piece.setPos(pos);
-            if (aStage.oven().isAbleToPut(piece, pos) &&
-                !PieceWithTurn(piece, aStage.turn()).collides(reservedPieces)) {
-              return Action::Put(CandidateLaneType_Small, order[i], pos);
-            }
-          }
-        }
-      } else {
-        for (int x = 0; x < Parameter::OvenWidth - piece.width() + 1; x++) {
-          for (int y = 0; y < Parameter::OvenHeight - piece.height() + 1; y++) {
-            Vector2i pos = Vector2i(x, y);
-            piece.setPos(pos);
-            if (aStage.oven().isAbleToPut(piece, pos) &&
-                !PieceWithTurn(piece, aStage.turn()).collides(reservedPieces)) {
-              return Action::Put(CandidateLaneType_Small, order[i], pos);
-            }
+      // 大きいクッキーの焼きあがりを待つ。
+      if (!gameWillEnd && piece.requiredHeatTurnCount() > minRestRequiredHeatTurnCount) {
+        continue;
+      }
+
+      for (int y = 0; y <= Parameter::OvenHeight - piece.height(); y++) {
+        for (int x = 0; x <= Parameter::OvenWidth - piece.width(); x++) {
+          Vector2i pos = Vector2i(x, y);
+          if (aStage.oven().isAbleToPut(piece, pos)) {
+            return Action::Put(CandidateLaneType_Small, order[i], pos);
           }
         }
       }
