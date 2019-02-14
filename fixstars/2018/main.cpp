ad0539c627bb99f82498c3d9e6f328c0d70d20a9
rdrgn <noreply@remin.jp>
2019-02-14 23:50:35 +0900

    update fixstars 2018

--- fixstars/2018/main.cpp
+++ fixstars/2018/main.cpp
@@ -1,200 +1,230 @@
-// akemi's Nodeless Solver (C) 2018 Fixstars Corp.
-// g++ akemi.cpp -std=c++14 -o akemi -O3 -Wall -Wno-unused-but-set-variable
+// remin's Nodeless Solver (C) 2018 remin
+// v1.0.0
 
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <vector>
-#include <map>
-#include <random>
-#include <algorithm>
-#include <utility>
-
+#include <bits/stdc++.h>
 #include "json.hpp"
 
-constexpr const char* version  = "0.04";
-constexpr const char* revision = "a";
-constexpr const char* ver_date = "20181211";
+// #define DEBUG_PRINT
 
-//#define DEBUG_PRINT
+class union_find {
+ private:
+  std::vector<int> parent;
+  std::vector<int> rank;
+  std::vector<int> sizes;
 
-void remove_newline(std::string& s)
-{
-	std::string target("\n");
-	std::string::size_type pos = s.find(target);
-	while (pos != std::string::npos) {
-		s.replace(pos, target.size(), "");
-		pos = s.find(target, pos);
-	}
+ public:
+  union_find(int n) : parent(n), rank(n), sizes(n, 1) {
+    for (int i = 0; i < n; i++) parent[i] = i;
+  }
+  void unite(int a, int b) {
+    a = find(a);
+    b = find(b);
+    if (a == b) return;
+    if (rank[a] < rank[b]) {
+      parent[a] = b;
+    } else {
+      parent[b] = a;
+      if (rank[a] == rank[b]) rank[a]++;
+    }
+    sizes[find(a)] = sizes[a] + sizes[b];
+  }
+  int find(int a) { return parent[a] == a ? a : parent[a] = find(parent[a]); }
+  int size(int a) { return sizes[find(a)]; }
+};
+
+void remove_newline(std::string& s) {
+  std::string target("\n");
+  std::string::size_type pos = s.find(target);
+  while (pos != std::string::npos) {
+    s.replace(pos, target.size(), "");
+    pos = s.find(target, pos);
+  }
 }
 
 struct Node {
-	std::vector<Node*> neighbors;
-    int idx;
-	int player;
-	int number;
-	int value;
+  std::vector<int> neighbors;
+  int index;
+  int player;
+  int count;
 
-    Node(int idx, int player, int number) : idx(idx), player(player), number(number) {
-		value = 0;
-	}
+  Node() : Node(0, -1, 0) {}
+  Node(int index, int player, int count) : index(index), player(player), count(count) {}
 };
 
-class Graph {
-public:
-	typedef std::map<int, Node*> nodes_t;
-    nodes_t nodes;
+struct Graph {
+  std::vector<Node> nodes;
 
-    void add_node(const int idx) {
-    	nodes_t::iterator it = nodes.find(idx);
-	    if (it == nodes.end()) {
-    	    Node *node;
-        	node = new Node(idx, -1, 0);
-        	nodes[idx] = node;
-        	return;
-    	}
-    	std::cerr << "Node already exists!" << std::endl;
-	}
-    void add_edge(const int from, const int to) {
-	    Node* f = (nodes.find(from)->second);
-	    Node* t = (nodes.find(to)->second);
-		f->neighbors.push_back(t);
-		t->neighbors.push_back(f);
-	}
-	int num_nodes() const { return static_cast<int>(nodes.size()); }
+  Graph() : Graph(0) {}
+  Graph(int size) { nodes = std::vector<Node>(size); }
+  int size() const { return static_cast<int>(nodes.size()); }
+  void add_edge(const int a, const int b) {
+    nodes[a].neighbors.push_back(b);
+    nodes[b].neighbors.push_back(a);
+  }
 };
 
-std::vector<int> initial_positions(Graph* G, int num_units)
-{
-	std::vector<int> positions;
-	std::vector<std::pair<int, Node*>> priority_nodes;
+struct Action {
+  int src;
+  int dst;
+  int count;
 
-	for (const auto& node : G->nodes) {
-		int num_neighbors = node.second->neighbors.size();
-		std::for_each(node.second->neighbors.begin(), node.second->neighbors.end(),
-			[num_neighbors](const auto& neighbor){ neighbor->value += 100 / num_neighbors; });
-	}
-	for (const auto& node : G->nodes) {
-		priority_nodes.push_back(std::make_pair(node.second->value, node.second));
-	}
-	std::sort(priority_nodes.begin(), priority_nodes.end(), [](const auto& a, const auto& b){ return a.first > b.first; });
-	for (const auto& node : priority_nodes) {
-		positions.push_back(node.second->idx);
-		if (static_cast<int>(positions.size()) >= num_units) break;
-	}
-	int remains = num_units - positions.size();
-	for (int i = 0; i < remains; i++) {
-		positions.push_back(positions.front());
-	}
-	std::sort(positions.begin(), positions.end());
+  Action(int src, int dst, int count) : src(src), dst(dst), count(count) {}
+};
 
-#ifdef DEBUG_PRINT
-	for (const auto& node : priority_nodes) {
-		std::cerr << node.second->value << ", " << node.second->idx << ", " << node.second->neighbors.size() << ", " << node.second->player << std::endl; ///// debug
-	}
-#endif // DEBUG_PRINT
+class Solver {
+ public:
+  Solver();
+  ~Solver();
+  std::vector<int> initialize(const Graph& graph, const int uid, const int unit_count);
+  Action update(const Graph& graph);
+  void finalize();
 
-	return positions;
+ private:
+  int uid;
+};
+
+Solver::Solver() {}
+
+Solver::~Solver() {}
+
+std::vector<int> Solver::initialize(const Graph& graph, const int uid, const int unit_count) {
+  this->uid = uid;
+
+  std::vector<int> positions;
+
+  std::vector<std::pair<int, int>> neighbor_counts;
+  for (const auto& node : graph.nodes) {
+    neighbor_counts.push_back({node.neighbors.size(), node.index});
+  }
+  std::sort(std::begin(neighbor_counts), std::end(neighbor_counts));
+  // auto it = std::upper_bound(std::begin(neighbor_counts), std::end(neighbor_counts), std::make_pair(unit_count, 0));
+  // if (it != std::begin(neighbor_counts)) it--;
+  // const auto& node_to_keep = graph.nodes[it->second];
+  const auto& node_to_keep = graph.nodes[neighbor_counts.front().second];
+  for (int i = 0; i < unit_count; i++) {
+    positions.push_back(node_to_keep.neighbors[i % node_to_keep.neighbors.size()]);
+  }
+
+  std::sort(std::begin(positions), std::end(positions));
+  return positions;
 }
 
-void solver(Graph* G, int& src, int& dst, int& num)
-{
-	std::vector<std::pair<int, Node*>> priority_nodes;
-	for (const auto& node : G->nodes) {
-		int value = std::count_if(node.second->neighbors.begin(), node.second->neighbors.end(),
-			[](const auto& neighbor){ return neighbor->player < 0; });
-		priority_nodes.push_back(std::make_pair(value, node.second));
-	}
-	std::sort(priority_nodes.begin(), priority_nodes.end(), [](const auto& a, const auto& b){ return a.first > b.first; });
+Action Solver::update(const Graph& graph) {
+  const int n = graph.size();
 
-	for (const auto& node : priority_nodes) {
-		if (node.second->number > 0 && !node.second->neighbors.empty()) {
-			for (const auto& neighbor : node.second->neighbors) {
-				if (neighbor->player == -1) {
-					src = node.second->idx;
-					dst = neighbor->idx;
-					num = node.second->number;
-					return;
-				}
-			}
-		}
-	}
-	src = -1;
-	dst = -1;
-	num = -1;
+  std::vector<int> empty_neighbor_count(n);
+  for (int i = 0; i < n; i++) {
+    for (auto e : graph.nodes[i].neighbors) {
+      empty_neighbor_count[i] += graph.nodes[e].player == -1;
+    }
+  }
+
+  std::vector<int> reachable_node_count(n);
+  {
+    union_find uf(n);
+    for (int i = 0; i < n; i++) {
+      if (graph.nodes[i].player != -1) continue;
+      for (auto e : graph.nodes[i].neighbors) {
+        if (graph.nodes[e].player != -1) continue;
+        uf.unite(i, e);
+      }
+    }
+    for (const auto& node : graph.nodes) {
+      union_find uf2 = uf;
+      for (int i : node.neighbors) {
+        uf2.unite(node.index, i);
+      }
+      reachable_node_count[node.index] = uf2.size(node.index) - 1;
+    }
+  }
+
+  std::vector<int> scores(n);
+  for (int i = 0; i < n; i++) {
+    scores[i] += empty_neighbor_count[i];
+    scores[i] += n * reachable_node_count[i];
+  }
+
+  Action action(-1, -1, -1);
+  {
+    std::pair<int, int> max_score = {0, 0};
+    for (int i = 0; i < n; i++) {
+      if (!(graph.nodes[i].player == this->uid && graph.nodes[i].count > 0)) continue;
+      for (int j : graph.nodes[i].neighbors) {
+        if (graph.nodes[j].player != -1) continue;
+        std::pair<int, int> score = {scores[j], -scores[i]};
+        if (score > max_score) {
+          max_score = score;
+          action = Action(i, j, graph.nodes[i].count);
+        }
+      }
+    }
+  }
+
+  return action;
 }
 
-int main(int argc, char* argv[])
-{
-	std::string s;
+void Solver::finalize() {}
 
-	Graph* G = new Graph();
+int main() {
+  Solver* solver = new Solver();
+  Graph graph;
+  std::string name;
 
-	for (;;) {
-		getline(std::cin, s);
-		nlohmann::json obj = nlohmann::json::parse(s);
-		auto action = obj["action"];
+  while (true) {
+    std::string s;
+    getline(std::cin, s);
+    nlohmann::json input = nlohmann::json::parse(s);
+    nlohmann::json output;
+    auto action = input["action"];
 
-		if (action == "play") {
-            auto nodes(obj["nodes"]);
-            auto edges(obj["edges"]);
-            auto state(obj["state"]);
-            auto units(obj["units"]);
+    if (action == "quit") {
+      solver->finalize();
+      std::cout << std::endl;
+      break;
+    }
 
-			for (int i = 0; i < G->num_nodes(); i++) {
-				G->nodes[i]->player = state[i];
-				G->nodes[i]->number = units[i];
-			}
+    auto nodes = input["nodes"];
+    auto edges = input["edges"];
+    auto state = input["state"];
+    auto units = input["units"];
 
-			int src;
-			int dst;
-			int num;
-			solver(G, src, dst, num);
+    if (action == "init") {
+      graph = Graph(nodes.size());
+      for (int i = 0; i < graph.size(); i++) {
+        graph.nodes[i].index = i;
+      }
+      for (const auto& edge : edges) {
+        graph.add_edge(edge[0], edge[1]);
+      }
+    }
+    for (int i = 0; i < graph.size(); i++) {
+      graph.nodes[i].player = state[i];
+      graph.nodes[i].count = units[i];
+    }
 
-			nlohmann::json out;
-			out["src"] = src;
-			out["dst"] = dst;
-			out["num"] = num;
-			std::string json(out.dump());
-			remove_newline(json);
-			std::cout << json << std::endl << std::flush;
+    if (action == "play") {
+      auto action = solver->update(graph);
 
+      output["src"] = action.src;
+      output["dst"] = action.dst;
+      output["num"] = action.count;
 #ifdef DEBUG_PRINT
-			std::cerr << "akemi: " << json << std::endl; ///// debug
-#endif // DEBUG_PRINT
+      std::cerr << name << ": " << output << std::endl;
+#endif
+    } else if (action == "init") {
+      int uid = input["uid"];
+      auto num_units = input["num_units"];
+      name = input["names"][uid];
 
-		} else if (action == "init") {
-			int uid(obj["uid"]);
-			auto names(obj["names"]);
-			auto name(names[uid]);
-			auto num_units(obj["num_units"]);
-            auto nodes(obj["nodes"]);
-            auto edges(obj["edges"]);
-            auto state(obj["state"]);
-            auto units(obj["units"]);
+      std::vector<int> positions = solver->initialize(graph, uid, num_units);
+      output["positions"] = positions;
+    }
 
-			for (const auto& node : nodes) {
-				G->add_node(node);
-			}
-			for (const auto& edge : edges) {
-				G->add_edge(edge[0], edge[1]);
-			}
+    s = output.dump();
+    remove_newline(s);
+    std::cout << s << std::endl;
+  }
 
-			std::vector<int> positions(initial_positions(G, num_units));
-
-			nlohmann::json out;
-			out["positions"] = positions;
-			std::string json(out.dump());
-			remove_newline(json);
-			std::cout << json << std::endl << std::flush;
-
-		} else if (action == "quit") {
-			std::cout << std::endl << std::flush;
-			break;
-		}
-	}
-
-	delete G;
-
-	return 0;
+  delete solver;
+  return 0;
 }
