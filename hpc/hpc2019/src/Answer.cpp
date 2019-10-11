277aaaaa7bf2b80a9dbfbf470027c2ef1b5d6603
remin <noreply@remin.jp>
2019-10-12 02:40:46 +0900

    update hpc2019 (96,089)

--- hpc/hpc2019/src/Answer.cpp
+++ hpc/hpc2019/src/Answer.cpp
@@ -1,83 +1,97 @@
-//------------------------------------------------------------------------------
-/// @file
-/// @author   ハル研究所プログラミングコンテスト実行委員会
-///
-/// @copyright  Copyright (c) 2019 HAL Laboratory, Inc.
-/// @attention  このファイルの利用は、同梱のREADMEにある
-///             利用条件に従ってください。
-//------------------------------------------------------------------------------
-
 #include "Answer.hpp"
+#include <ctime>
+#include <vector>
 
-//------------------------------------------------------------------------------
+constexpr int TIME_LIMIT = 60 * 1000;
+
+class Timer {
+ public:
+  Timer() : time_begin(std::clock()) {}
+  int Elapsed() { return 1000 * (std::clock() - time_begin) / CLOCKS_PER_SEC; }
+
+ private:
+  std::clock_t time_begin;
+};
+
 namespace hpc {
 
-//------------------------------------------------------------------------------
-/// コンストラクタ。
-/// @detail 最初のステージ開始前に実行したい処理があればここに書きます。
-Answer::Answer()
-{
+inline int distance(Point a, Point b) { return Math::Abs(b.x - a.x) + Math::Abs(b.y - a.y); }
+
+Random random(RandomSeed::DefaultSeed());
+::Timer timer;
+int stage_index;
+std::vector<int> route;
+
+Answer::Answer() {
+  timer = ::Timer();
+  stage_index = 0;
 }
 
-//------------------------------------------------------------------------------
-/// デストラクタ。
-/// @detail 最後のステージ終了後に実行したい処理があればここに書きます。
-Answer::~Answer()
-{
-}
+Answer::~Answer() {}
 
-//------------------------------------------------------------------------------
-/// 各ステージ開始時に呼び出される処理。
-/// @detail 各ステージに対する初期化処理が必要ならここに書きます。
-/// @param aStage 現在のステージ。
-void Answer::initialize(const Stage& aStage)
-{
-}
+void Answer::initialize(const Stage& aStage) {
+  const auto& foods = aStage.foods();
+  int food_count = foods.count();
 
-//------------------------------------------------------------------------------
-/// 各ターンのカメの行動を指定する。
-/// @detail 各カメの行動を指定し、aActionsの要素に追加してください。
-///         aActions[i]がi番目のカメの行動になります。
-///         aActionsの要素数がaStage.turtlePositions()の要素数と異なる場合、アサートに失敗します。
-/// @param[in] aStage 現在のステージ。
-/// @param[out] aActions 各カメの行動を指定する配列。
-void Answer::setActions(const Stage& aStage, Actions& aActions)
-{
-    // 解答コードのサンプルです。
+  route = std::vector<int>(aStage.foods().count());
+  for (int i = 0; i < food_count; i++) route[i] = i;
 
-    // まだ食べられていないエサを1つ選んで目的地に設定する。
-    Point targetFoodPosition;
-    for (Food food : aStage.foods()) {
-        if (!food.isEaten()) {
-            targetFoodPosition = food.pos();
-            break;
-        }
+  int length = 0;
+  for (int i = 0; i < food_count; i++) {
+    length += distance(foods[route[i]].pos(), foods[route[(i + 1) % food_count]].pos());
+  }
+
+  while (timer.Elapsed() + 20 < TIME_LIMIT / Parameter::GameStageCount * (stage_index + 1)) {
+    int a1 = random.randTerm(food_count);
+    int b1;
+    do {
+      b1 = random.randTerm(food_count);
+      if (b1 < a1) b1 += food_count;
+    } while (b1 - a1 < 2);
+    int a2 = a1 + 1, b2 = b1 + 1;
+
+    int delta_length = 0;
+    delta_length -= distance(foods[route[a1 % food_count]].pos(), foods[route[a2 % food_count]].pos());
+    delta_length -= distance(foods[route[b1 % food_count]].pos(), foods[route[b2 % food_count]].pos());
+    delta_length += distance(foods[route[a1 % food_count]].pos(), foods[route[b1 % food_count]].pos());
+    delta_length += distance(foods[route[a2 % food_count]].pos(), foods[route[b2 % food_count]].pos());
+
+    if (delta_length < 0) {
+      length += delta_length;
+      for (int i = a2, j = b1; i < j; i++, j--) {
+        int t = route[i % food_count];
+        route[i % food_count] = route[j % food_count];
+        route[j % food_count] = t;
+      }
     }
+  }
+}
 
-    // すべてのカメを目的地に向かって移動させる。
-    for (Point turtlePosition : aStage.turtlePositions()) {
-        if (turtlePosition.x < targetFoodPosition.x) {
-            aActions.add(Action_MoveRight);
-        } else if (turtlePosition.x > targetFoodPosition.x) {
-            aActions.add(Action_MoveLeft);
-        } else if (turtlePosition.y < targetFoodPosition.y) {
-            aActions.add(Action_MoveDown);
-        } else if (turtlePosition.y > targetFoodPosition.y) {
-            aActions.add(Action_MoveUp);
-        } else {
-            // 移動する必要がなければ、その場で待機する。
-            aActions.add(Action_Wait);
-        }
+void Answer::setActions(const Stage& aStage, Actions& aActions) {
+  Point targetFoodPosition;
+  for (int i = 0; i < aStage.foods().count(); i++) {
+    const auto& food = aStage.foods()[route[i]];
+    if (!food.isEaten()) {
+      targetFoodPosition = food.pos();
+      break;
     }
+  }
+
+  for (Point turtlePosition : aStage.turtlePositions()) {
+    if (turtlePosition.x < targetFoodPosition.x) {
+      aActions.add(Action_MoveRight);
+    } else if (turtlePosition.x > targetFoodPosition.x) {
+      aActions.add(Action_MoveLeft);
+    } else if (turtlePosition.y < targetFoodPosition.y) {
+      aActions.add(Action_MoveDown);
+    } else if (turtlePosition.y > targetFoodPosition.y) {
+      aActions.add(Action_MoveUp);
+    } else {
+      aActions.add(Action_Wait);
+    }
+  }
 }
 
-//------------------------------------------------------------------------------
-/// 各ステージ終了時に呼び出される処理。
-/// @detail 各ステージに対する終了処理が必要ならここに書きます。
-/// @param aStage 現在のステージ。
-void Answer::finalize(const Stage& aStage)
-{
-}
+void Answer::finalize(const Stage& aStage) { stage_index++; }
 
-} // namespace
-// EOF
+}  // namespace hpc
