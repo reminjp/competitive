3b3f63e736548c5ff8a8eb839c88acadb8a85d44
remin <noreply@remin.jp>
2019-12-09 02:55:35 +0900

    update abc147 E

--- atcoder/abc/abc147/e.cpp
+++ atcoder/abc/abc147/e.cpp
@@ -12,25 +12,20 @@ int main() {
     for (auto& e : v) cin >> e;
   }
 
-  int m = 80 * 80 + 1;
-  vector<vector<vector<bool>>> s(h + 1, vector<vector<bool>>(w + 1, vector<bool>(m)));
-  s[0][0][0] = true;
-  s[0][1][0] = true;
-  s[1][0][0] = true;
+  constexpr int S = 80 * 80;
+  vector<vector<bitset<2 * S + 1>>> s(h + 1, vector<bitset<2 * S + 1>>(w + 1));
+  s[0][0].set(S);
+  s[0][1].set(S);
+  s[1][0].set(S);
   for (int i = 0; i < h; i++) {
     for (int j = 0; j < w; j++) {
       int d = abs(a[i][j] - b[i][j]);
-      for (int k = 0; k < m; k++) {
-        if (!s[i + 1][j][k]) continue;
-        if (abs(k + d) < m) s[i + 1][j + 1][abs(k + d)] = true;
-        if (abs(k - d) < m) s[i + 1][j + 1][abs(k - d)] = true;
-      }
-      for (int k = 0; k < s[i][j + 1].size(); k++) {
-        if (!s[i][j + 1][k]) continue;
-        if (abs(k + d) < m) s[i + 1][j + 1][abs(k + d)] = true;
-        if (abs(k - d) < m) s[i + 1][j + 1][abs(k - d)] = true;
-      }
+      s[i + 1][j + 1] |= s[i][j + 1] << d;
+      s[i + 1][j + 1] |= s[i][j + 1] >> d;
+      s[i + 1][j + 1] |= s[i + 1][j] << d;
+      s[i + 1][j + 1] |= s[i + 1][j] >> d;
     }
   }
-  cout << find(s[h][w].begin(), s[h][w].end(), true) - s[h][w].begin() << endl;
+  string ss = (s[h][w] >> S).to_string();
+  cout << find(ss.rbegin(), ss.rend(), '1') - ss.rbegin() << endl;
 }
