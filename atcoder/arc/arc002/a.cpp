9beda2764bca27f8cba8e6bb5b8b7f9d1d0e40a7
remin <noreply@remin.jp>
2019-12-31 22:02:44 +0900

    update arc002 A

--- atcoder/arc/arc002/a.cpp
+++ atcoder/arc/arc002/a.cpp
@@ -1,19 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+bool IsLeapYear(int y) { return y % 400 == 0 || (y % 100 != 0 && y % 4 == 0); }
+
 int main() {
-	int y;
-	cin >> y;
-	bool r;
-	if (y % 400 == 0) {
-		r = true;
-	} else if (y % 100 == 0) {
-		r = false;
-	} else if (y % 4 == 0) {
-		r = true;
-	} else {
-		r = false;
-	}
-	cout << (r ? "YES" : "NO") << endl;
-	return 0;
+  int y;
+  cin >> y;
+  cout << (IsLeapYear(y) ? "YES" : "NO") << endl;
 }
