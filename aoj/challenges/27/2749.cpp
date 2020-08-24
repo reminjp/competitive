034ee415df5c07c86d9acc94e9b5ead92ccc6810
remin <noreply@remin.jp>
2020-08-24 18:36:35 +0900

    update AOJ 2749

--- aoj/challenges/27/2749.cpp
+++ aoj/challenges/27/2749.cpp
@@ -1,39 +1,35 @@
-// 2017-07-13
+// 2020-08-24
 #include <algorithm>
 #include <cmath>
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main() {
-	int n, m, s[15], d[100], sum[1 << 15], dp[1 << 15];
-	while (cin >> n >> m, n) {
-		for (int i = 0; i < n; i++) {
-			cin >> s[i];
-		}
-		for (int i = 0; i < m; i++) {
-			cin >> d[i];
-		}
-		sort(d, d + m);
-		for (int i = 0; i < 1 << n; i++) {
-			sum[i] = 0;
-			for (int j = 0; j < n; j++) {
-				if (i & 1 << j) {
-					sum[i] += s[j];
-				}
-			}
-		}
-		fill(dp, dp + (1 << n), 0);
-		for (int i = 0; i < m; i++) {
-			for (int j = 0; j < 1 << n; j++) {
-				dp[j] += abs(sum[j] - d[i]);
-			}
-			for (int j = 0; j < 1 << n; j++) {
-				for (int k = 0; k < n; k++) {
-					dp[j] = min(dp[j], dp[j & ~(1 << k)]);
-				}
-			}
-		}
-		cout << dp[(1 << n) - 1] << endl;
-	}
-	return 0;
+  for (int n, m; cin >> n >> m, n;) {
+    vector<int> s(n), d(m);
+    for (auto &e : s) cin >> e;
+    for (auto &e : d) cin >> e;
+    sort(d.begin(), d.end());
+
+    vector<int> sum(1 << n);
+    for (int j = 0; j < 1 << n; j++) {
+      for (int k = 0; k < n; k++) {
+        if (j & 1 << k) sum[j] += s[k];
+      }
+    }
+
+    vector<int> dp(1 << n);
+    for (int i = 0; i < m; i++) {
+      for (int j = 0; j < 1 << n; j++) {
+        dp[j] += abs(sum[j] - d[i]);
+      }
+      for (int j = 0; j < 1 << n; j++) {
+        for (int k = 0; k < n; k++) {
+          dp[j] = min(dp[j], dp[j & ~(1 << k)]);
+        }
+      }
+    }
+    cout << dp[(1 << n) - 1] << endl;
+  }
 }
